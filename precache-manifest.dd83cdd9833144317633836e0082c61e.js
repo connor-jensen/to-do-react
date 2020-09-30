e36436b8e5579fe2b71912aa22e11532c6ebee29
Connor Jensen <connor.jensen@live.com>
2020-09-29 21:09:06 -0600

    Updates

---
+++ precache-manifest.dd83cdd9833144317633836e0082c61e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11fdd0669b311ca3773c5346a0e51a2f",
+    "url": "/%7Bto-do-react%7D/index.html"
+  },
+  {
+    "revision": "24116def6f4afa594d4d",
+    "url": "/%7Bto-do-react%7D/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "fa21f013b0f5f211a376",
+    "url": "/%7Bto-do-react%7D/static/css/main.62764f7d.chunk.css"
+  },
+  {
+    "revision": "24116def6f4afa594d4d",
+    "url": "/%7Bto-do-react%7D/static/js/2.f2f27158.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/%7Bto-do-react%7D/static/js/2.f2f27158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa21f013b0f5f211a376",
+    "url": "/%7Bto-do-react%7D/static/js/main.a5a2c5cf.chunk.js"
+  },
+  {
+    "revision": "14f92ce89281e27bea4d",
+    "url": "/%7Bto-do-react%7D/static/js/runtime-main.cd80dcc9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/%7Bto-do-react%7D/precache-manifest.dd83cdd9833144317633836e0082c61e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7Bto-do-react%7D/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
